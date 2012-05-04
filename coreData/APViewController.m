90317dfb049df90e3ccf9314964ef62afc01a5ac
Alan Stirling <alan@johnstonnorth.com>
2012-05-04 10:23:55 +0100

    started feed

--- coreData/APViewController.m
+++ coreData/APViewController.m
@@ -9,8 +9,11 @@
 #import "APViewController.h"
 #import "APAppDelegate.h"
 #import "Content.h"
+#import "Person.h"
 
 @interface APViewController ()
+
+- (void) getFeed;
 
 @end
 
@@ -22,11 +25,33 @@
 @synthesize contentLabel = _contentLabel;
 
 
+-(void) getFeed
+{
+    //NSURLRequest * request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://feed.astirling.com/feed.json"]];
+    NSError * e = nil;
+    NSData * data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://feed.astirling.com/feed.json"]];
+    NSDictionary * feed = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error: &e];
+    
+    if(!feed){
+        NSLog(@"Error - %@", e);
+    }
+    else {
+        for(id entry in feed){
+            Person * newPerson = [NSEntityDescription insertNewObjectForEntityForName:@"Person" inManagedObjectContext:self.managedObjectContext];
+            
+            newPerson.first_name = [entry valueForKey:@"first_name"];
+            newPerson.last_name = [entry valueForKey:@"last_name"];
+            newPerson.email = [entry valueForKey:@"email"];
+            newPerson.date_of_birth = (NSDate *)[entry valueForKey:@"date_of_birth"];
+        }
+    }
+}
 
 - (void)viewDidLoad
 {
     //get managed object context from APAppDelegate
     self.managedObjectContext = ((APAppDelegate *)UIApplication.sharedApplication.delegate).managedObjectContext;
+    
 
     // set the contents of the label to the body of the object from the resultsController at index 0 (the first object)
     NSIndexPath * indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
@@ -75,7 +100,7 @@
         
     }
     
-    
+    /*
     // this section is just to populate the the results controller if it is empty
     if([_resultsController.fetchedObjects count] < 1){
         
@@ -97,7 +122,7 @@
         }
     }
     //---------------------------------------------------------------------------
-    
+    */
 
     return _resultsController;
 }
